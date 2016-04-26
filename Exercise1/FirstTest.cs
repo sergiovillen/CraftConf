2498720dc5c4b9009129e4aaef16aefab0676b20
Sergio <sergio.villen.galera@hotmail.fr>
2016-04-26 09:48:52 +0200

    Failing test

---
+++ Exercise1/FirstTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace Exercise1
+{
+    public class FirstTest
+    {
+        [Test]
+        public void fake_test()
+        {
+            var result = false;
+            Check.That(result).IsTrue();
+        }
+    }
+}
