2a93705fde8d29d4e94bd957498fc4163626cf86
Sergio <sergio.villen.galera@hotmail.fr>
2016-04-26 10:29:10 +0200

    throw

---
+++ Stack/StackShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace StackProject
+{
+    public class StackShould
+    {
+        [Test]
+        public void throw_exception_if_popped_when_empty()
+        {
+            var stack = new Stack();
+
+            Assert.Throws<Exception>(() =>
+            {                
+                stack.Pop();
+            });
+        }
+    }
+
+    internal class Stack
+    {
+        public void Pop()
+        {
+            throw new Exception();
+        }
+    }
+}
